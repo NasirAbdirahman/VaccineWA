e2e60e11079e97333defa26aa704e432af049804
AbdulNasir Abdirahman <nasir.a.abdirahman@outlook.com>
2021-03-22 21:26:42 -0700

    Home COmponent Input Fields

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Main from './components/MainComponent.js';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Scheherazade_700Bold } from '@expo-google-fonts/scheherazade';
+import { SourceSansPro_600SemiBold } from '@expo-google-fonts/source-sans-pro';
+import { Lateef_400Regular } from '@expo-google-fonts/lateef';
+
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     Scheherazade_700Bold,
+    SourceSansPro_600SemiBold,
+    Lateef_400Regular
   });
 
   if (!fontsLoaded) {
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { Button} from 'react-native-elements';
-
+import { Input, Button, Divider} from 'react-native-elements';
 
 
 class Home extends Component {
@@ -14,12 +13,32 @@ class Home extends Component {
     render () {
         const { navigate } = this.props.navigation;
         return (
-            <View  style={{paddingTop:10}}>
+            <View  style={{paddingTop:10,paddingBottom:20, backgroundColor: '#ffffff'}}>
 
                 <Text style={styles.HeaderTitle}>Find A <Text style={styles.HeaderTitle2}>Covid-19 </Text>Vaccine Near You</Text>
+
+                <Divider style={{ backgroundColor: '#B1DDF9', width:'85%', alignSelf:'center', bottom:10, padding:0.5}}/>
+                <Text style={styles.Text}>Type A 5-Digit Zip Code</Text>
+                <Input placeholder='Zip Code' style={styles.Input}/>
+                <Text style={styles.Text2}>Within</Text>
                 
+                <Input 
+                    placeholder='10 Miles' 
+                    style={styles.Input}
+                    rightIcon={{ type: 'material-icons', name: 'arrow-drop-down' , size:30}}
+                     
+                />
+                
+               
+               
+                
+                
+
                 <Button //Add this to all the information Pages
-                    title="Search Providers"
+                    buttonStyle={styles.Button}
+                    containerStyle={styles.ButtonContainer}
+                    titleStyle={styles.Button}
+                    title="Search For Vaccines"
                     onPress={() => navigate('Results')}> 
                 </Button>
                 
@@ -31,21 +50,61 @@ class Home extends Component {
 
 
 const styles= StyleSheet.create({
-    HeaderTitle:{
+    HeaderTitle: {
         color: '#000',
         textAlign:'center',
         fontSize: 27,
         fontFamily:'Scheherazade_700Bold',
-        letterSpacing: 1
+        letterSpacing: 1,
     },
-    HeaderTitle2:{
+    HeaderTitle2: {
         color: '#70BAFF',
         textAlign:'center',
         fontSize: 35,
         letterSpacing: 1,
         
+    },
+    Text: {
+        fontSize: 20,
+        fontFamily:'SourceSansPro_600SemiBold',
+        letterSpacing: 1,
+        paddingLeft:5,
+        paddingTop:50,
+      
+        
+    },
+    Text2: {
+        fontSize: 20,
+        fontFamily:'SourceSansPro_600SemiBold',
+        letterSpacing: 1,
+        paddingLeft:5,
+        paddingTop:7
+        
+    },
+    Button: {
+        backgroundColor:'#70BAFF',
+        borderRadius:25,
+        padding:4,
+        paddingTop:6,
+        paddingBottom:6,
+        alignSelf: "center",
+        fontSize: 25,
+  
+    },
+    ButtonContainer: {
+        elevation:15,
+        alignSelf: "center",
+        backgroundColor:'#B1DDF9',
+        shadowColor:'#70BAFF',
+        padding:1.5,
+        borderRadius:25,
+    },
+    Input: {
+        paddingTop:10,
+        fontFamily:'SourceSansPro_600SemiBold',
+        letterSpacing: 1,
     }
-
+    
 })
 
 
