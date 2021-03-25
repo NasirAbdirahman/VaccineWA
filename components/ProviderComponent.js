bea127370b468a230ab702e5c0c6c38978a015ac
AbdulNasir Abdirahman <nasir.a.abdirahman@outlook.com>
2021-03-25 13:51:05 -0700

    Added Vaccine Type

--- components/ProviderComponent.js
+++ components/ProviderComponent.js
@@ -4,7 +4,89 @@ import { Divider, Icon} from 'react-native-elements';
 import { PROVIDERDATA } from '../shared/ProviderData'; 
 
         
-    
+function RenderVaccineType({providerdata}) {
+
+    const renderVaccine = ({item}) => {
+            if (item.vaccineTypes.includes("Moderna") && item.availability === "Yes") {
+                return (
+                    <View>
+                        <Text style={styles.Text}>Moderna Covid Vaccine </Text>
+            
+                        <Icon 
+                            name={'check-circle'}
+                            type= 'material-icons' 
+                            color='#70BAFF'
+                        /> 
+                    </View>
+                )
+            } else if ( item.vaccineTypes.includes("Pfizer") && item.availability === "Yes") {
+                return (
+                    <View>
+                        <Text style={styles.Text}>Pfizer/BioNTech Covid Vaccine </Text>
+                        <Icon 
+                            name={'check-circle'}
+                            type= 'material-icons' 
+                            color='#70BAFF'
+                        /> 
+                    </View>
+                )
+            } else if ( item.vaccineTypes.includes("J&J/Janssen") && item.availability === "Yes" ) {
+                return(
+                    <View>
+                        <Text style={styles.Text}>Johnson & Johnson’s Janssen Covid Vaccine</Text>
+                        <Icon 
+                            name={'check-circle'}
+                            type= 'material-icons' 
+                            color='#70BAFF'
+                        /> 
+                    </View>
+                )
+            } else if (item.vaccineTypes.includes("Unknown") && item.availability === "Yes") {
+                return (
+                    <View style={{paddingLeft:10}}>
+                        <Text style={styles.Text}>Unknown : Vaccines are available but the Provider has not stated the type</Text>
+                        <Text>In Stock:
+                            <Icon 
+                                name={'check-circle'}
+                                type= 'material-icons' 
+                                color='#70BAFF'
+                            /> 
+                        </Text>
+                    </View>
+                )
+            } else if (item.availability === "NO")  { 
+                return (
+                <View>
+                    <Text style={styles.Text}> Vaccines Are Currently UnAvailable </Text>
+                    <Icon 
+                        name={'check-circle'}
+                        type= 'material-icons' 
+                        color='#70BAFF'
+                    /> 
+                </View> 
+                )
+            } else { //May not need this code//
+                return (
+                <View/>
+                ) //May not need this code//
+            }
+                 
+
+    };
+
+
+    return (
+        <View>
+            <FlatList
+                data={providerdata}
+                renderItem={renderVaccine}
+                keyExtractor={item => item.id.toString()} //Because all the Providers have a unique ID, we can set this to use the ID//
+            />
+        </View>
+
+    );
+
+}   
 
 function RenderProvider ({providerdata}) {
 
@@ -23,12 +105,12 @@ function RenderProvider ({providerdata}) {
 
                 <View>
                     <Text style={styles.HeaderTitle}>Vaccine Types Available At This Location</Text>
-                        <View>
+                       {/*  <View>
                             <Text style={styles.Text}>Moderna Covid Vaccine </Text>
                             <Text> 
                                 In Stock:
                                 <Icon 
-                                    name={item.vaccineTypes.includes("Moderna") && item.availability === "Yes" || item.vaccineTypes === "Unknown" && item.availability === "Yes" /* Allows Checkss to show even if vaccinetype is unknown. fixed bug that showed 'x' when type was unknown but vaccines avaiible*/ ? 'check-circle' : 'highlight-off' }//before includes(),using ==/=== did not work due to data array not seperating vaccinesType name//
+                                    name={item.vaccineTypes.includes("Moderna") && item.availability === "Yes" || item.vaccineTypes === "Unknown" && item.availability === "Yes" // Allows Checkss to show even if vaccinetype is unknown. fixed bug that showed 'x' when type was unknown but vaccines avaiible ? 'check-circle' : 'highlight-off' }//before includes(),using ==/=== did not work due to data array not seperating vaccinesType name//
                                     type= 'material-icons' 
                                     color={item.vaccineTypes.includes("Moderna") && item.availability === "Yes" || item.vaccineTypes === "Unknown" && item.availability === "Yes" ? Icon.color='#70BAFF': Icon.color='#FF0000'}
                                 /> 
@@ -53,8 +135,8 @@ function RenderProvider ({providerdata}) {
                                 /> 
                             </Text> 
                         </View>
-                 {/* All Links will go here..Will need to use the navigation props*/}
-                </View>
+                 {// All Links will go here..Will need to use the navigation props//} */}
+                </View> 
 
             </View>
                  
@@ -102,6 +184,7 @@ class Provider extends Component {
         return (
             <View style={{paddingTop:10,paddingBottom:20, backgroundColor: '#ffffff'}}>
                 <RenderProvider providerdata={providerdata}/>
+                <RenderVaccineType providerdata={providerdata}/>
                
                 
             </View>
