27b91f280302eb09dfbc1b8544384e5632e8c492
AbdulNasir Abdirahman <nasir.a.abdirahman@outlook.com>
2021-03-28 21:55:53 -0700

    Eligibility Component Complete

--- components/EligibilityComponent.js
+++ components/EligibilityComponent.js
@@ -11,69 +11,68 @@ class Eligibility extends Component {
     };
 
     render () {
-
-      
-
-
         return (
            
             <ScrollView style={{paddingTop:15,paddingBottom:20, backgroundColor: '#ffffff'}}>
 
-                <View style={{paddingLeft:5, paddingBottom:30}}>
-                    <Text style={styles.HeaderTitle}>Check your Covid-19 Vaccine Eligibility</Text>
-                    <Text style={styles.Text}>By Answering the Official State Phase Finder Questionnaire, you can see when You are eligible to receive the vaccine.</Text>
-                    <Button 
-                        buttonStyle={styles.Button}
-                        containerStyle={styles.ButtonContainer}
-                        titleStyle={styles.Button}
-                        
-                        title="Start"
-                        //onPress={() =>  navigation.goBack()}
-                        > 
-                    </Button>
+                <View style={{paddingLeft:7, paddingBottom:15}}>      
+                    <Text style={styles.HeaderTitle}>Check your Covid-19 Vaccine Eligibility</Text>          
+                    <Text style={styles.Text}>By Answering the <Text style={styles.Text2}>Official State Phase Finder</Text> Questionnaire, you can see when You are eligible to receive the vaccine.</Text>  
                 </View>
 
-                <Divider style={{ backgroundColor: '#B1DDF9', width:'85%', alignSelf:'center', top:18, padding:1}}/>
+                <Button 
+                    buttonStyle={styles.Button}
+                    containerStyle={styles.ButtonContainer}
+                    titleStyle={styles.Button}
+                    
+                    title="Start"
+                    //onPress={() =>  navigation.goBack()}
+                    > 
+                </Button>
 
-                <View style={{paddingLeft:5, paddingBottom:30}}>
+                <Divider style={{ backgroundColor: '#B1DDF9', width:'100%', alignSelf:'center', margin:10, padding:1}}/>
+
+                <View style={{paddingLeft:7, paddingBottom:30}}>
                     <Text style={styles.Title}>Why A Questionnaire?</Text>
                     <Text style={styles.Text}>Due to a limited vaccine supply, we need to provide the vaccine in phases. 
-                        We are starting with people who are most at risk such as healthcare workers, elders, vulnerable individuals, and high-risk critical workers. Our goal is for this process to be as fair and transparent as possible. 
-                        Visit doh.wa.gov/VaccinationPhasesInfographic.pdf to find out more about Washington's Covid-19 Vaccine Phases.
-                    </Text>
-                    <Divider style={{ backgroundColor: '#B1DDF9', width:'85%', alignSelf:'center', top:18, padding:1}}/>
+                        We are starting with <Text style={styles.Text2}>people who are most at risk such as healthcare workers, elders, vulnerable individuals, and high-risk critical workers.</Text> Our goal is for this process to be as fair and transparent as possible. 
+                    </Text> 
+                    <Text style={styles.Text}>Visit <Text /*NEEDS TO LINK TO Pdf*/style={styles.Link}>doh.wa.gov/VaccinationPhasesInfographic.pdf</Text> to find out more about Washington's Covid-19 Vaccine Phases.</Text>
+                    <Text style={styles.Text}>Or View<Text /*NEEDS TO LINK TO Phases Page*/style={styles.Link}> Washington's Covid-19 Vaccine Phases On The App </Text></Text>
+                    
                 </View>
 
-                <Text style={styles.Title}>Why A Questionnaire?</Text>
-                <Text style={styles.Text}>Due to a limited vaccine supply, we need to provide the vaccine in phases. 
-                    We are starting with people who are most at risk such as healthcare workers, elders, vulnerable individuals, and high-risk critical workers. Our goal is for this process to be as fair and transparent as possible. 
-                    Visit doh.wa.gov/VaccinationPhasesInfographic.pdf to find out more about Washington's Covid-19 Vaccine Phases.
-                </Text>
-                <Divider style={{ backgroundColor: '#B1DDF9', width:'85%', alignSelf:'center', top:18, padding:1}}/>
+                <Divider style={{ backgroundColor: '#B1DDF9', width:'85%', alignSelf:'center', margin:10, padding:1}}/>
 
-                <Text style={styles.Title}>Who can Receive The Vaccine?</Text>
-                <Text style={styles.Text}>Everyone will be eligible to receive the COVID-19 vaccine over the coming months. The COVID-19 vaccine is free for all, regardless of whether you have private health insurance, are uninsured, or are on Medicare.
-                    We all will benefit if those who face the highest risk get vaccinated first. This will reduce the burden on hospitals, slow the spread of COVID, and help us reopen our communities faster. 
-                    We are committed to ensuring that every eligible person in Washington will get vaccinated.
-                </Text>
-                <Divider style={{ backgroundColor: '#B1DDF9', width:'85%', alignSelf:'center', top:18, padding:1}}/>
+                <View style={{paddingLeft:7, paddingBottom:30}}>
+                    <Text style={styles.Title}>Who can Receive The Vaccine?</Text>
+                    <Text style={styles.Text}><Text style={styles.Text2}>Everyone will be eligible to receive the COVID-19 vaccine over the coming months.</Text> </Text>
+                    <Text style={styles.Text2}>The COVID-19 vaccine is free for all, regardless of whether you have private health insurance, are uninsured, or are on Medicare.</Text>
+                    <Text style={styles.Text}><Text style={styles.Text2}>We all will benefit if those who face the highest risk get vaccinated first.</Text> This will reduce the burden on hospitals, slow the spread of COVID, and help us reopen our communities faster. 
+                        We are committed to ensuring that every eligible person in Washington will get vaccinated.</Text>  
+                </View>
 
-                <Text style={styles.Title}>How do I know if I am Eligible?</Text>
-                <Text style={styles.Text}>Please complete the following questions to determine whether you are eligible to get the COVID-19 vaccine now. 
-                    Please know we will NOT share your information with anyone. We will use it to assess eligibility and inform planning.
-                    If you are eligible, you can use the confirmation page to show your eligibility when you go to get vaccinated. The confirmation page is not an appointment. You will still need to make an appointment with a vaccine provider. 
-                </Text>
-                <Divider style={{ backgroundColor: '#B1DDF9', width:'85%', alignSelf:'center', top:18, padding:1}}/>
+                <Divider style={{ backgroundColor: '#B1DDF9', width:'85%', alignSelf:'center', margin:10, padding:1}}/>
 
-                <Text style={styles.Title}>How About If I Am Not Eligible?</Text>
-                <Text style={styles.Text}>If you are not eligible yet, please provide contact information and the Washington State Department of Health will provide updates on COVID vaccinations and notify you when you become eligible. 
-                    We can contact you via phone, text, email or through this App and let you know when you’re eligible to receive the vaccine.
-                    Or you can also check back later.
-                </Text>
-                <Divider style={{ backgroundColor: '#B1DDF9', width:'85%', alignSelf:'center', top:18, padding:1}}/>
+                <View style={{paddingLeft:7, paddingBottom:30}}>
+                    <Text style={styles.Title}>How do I know if I am Eligible?</Text>
+                    <Text style={styles.Text}>Please complete the following questions to determine whether you are eligible to get the COVID-19 vaccine now. </Text>
+                    <Text style={styles.Text2}>Please know we will NOT share your information with anyone. We will use it to assess eligibility and inform planning.</Text>
+                    <Text style={styles.Text2}>If you are eligible, you can use the confirmation page to show your eligibility when you go to get vaccinated. The confirmation page is not an appointment. You will still need to make an appointment with a vaccine provider. 
+                    </Text>
+                </View>
+
+                <Divider style={{ backgroundColor: '#B1DDF9', width:'85%', alignSelf:'center', margin:10, padding:1}}/>
+
+                <View style={{paddingLeft:7, paddingBottom:30}}>
+                    <Text style={styles.Title}>How About If I Am Not Eligible?</Text>
+                    <Text style={styles.Text}>If you are not eligible yet, please provide contact information and the Washington State Department of Health will provide updates on COVID vaccinations and notify you when you become eligible.</Text>
+                    <Text style={styles.Text2}>We can contact you via phone, text, email or through this App and let you know when you’re eligible to receive the vaccine.</Text>
+                    <Text style={styles.Text}>Or you can also check back later.</Text>
+                    
+                </View>
 
             </ScrollView>
-
         )
     }
 }
@@ -111,7 +110,7 @@ const styles = StyleSheet.create({
         letterSpacing: 1,
     },*/
     Text: {
-        fontSize: 16,
+        fontSize: 15,
         fontFamily:'SourceSansPro_600SemiBold',
         letterSpacing: 1,
         paddingTop:10,
@@ -119,15 +118,15 @@ const styles = StyleSheet.create({
       
         
     },
-    /*Text2: {
-        fontSize: 16,
+    Text2: {
+        fontSize: 15,
         fontFamily:'SourceSansPro_700Bold',
         letterSpacing: 1,
         paddingTop:7,
         color:'#000'//#70BAFF-Alt. Color//
         
         
-    },*/
+    },
     Button: {
         backgroundColor:'#70BAFF',
         borderRadius:25,
@@ -136,7 +135,7 @@ const styles = StyleSheet.create({
         paddingBottom:6,
         alignSelf: "center",
         fontSize: 20,
-        //bottom:1.5 = reveals light blue bg//
+        //bottom:1.7 = reveals light blue bg//
   
     },
     ButtonContainer: {
@@ -144,8 +143,15 @@ const styles = StyleSheet.create({
         alignSelf: "center",
         backgroundColor:'#B1DDF9',
         shadowColor:'#70BAFF',
-        padding:1.5,
+        padding:1.7,
         borderRadius:25,
+    },
+    Link:{
+        fontSize: 15,
+        fontFamily:'SourceSansPro_700Bold',
+        color: '#70BAFF',
+        paddingTop:10
+        
     },
     
 });
